4e5effe12f8d9d864b4371714f7a6683d0f6aecc
Robert Jandow <robert.jandow@tum.de>
2023-03-30 12:07:09 +0300

    Improve MQTT for new structure

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
--- mqtt.go
+++ mqtt.go
@@ -8,20 +8,17 @@ import (
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 )
 
 var MQTTClient mqtt.Client
 
-func handle_sub(_ mqtt.Client, msg mqtt.Message) {
-	log.Infof("%s", msg.Topic())
-	log.Infof("%s", msg.Payload())
-}
-
-func handle_pub(_ mqtt.Client, msg mqtt.Message) {
-	log.Infof("%s", msg.Topic())
-	log.Infof("%s", msg.Payload())
-}
+const (
+	AT_MOST_ONCE  = 0
+	AT_LEAST_ONCE = 1
+	EXACTLY_ONCE  = 2
+)
 
 func InitMQTTClient() {
 	opts := mqtt.NewClientOptions().AddBroker(Cfg.MQTT.Broker).SetClientID(Cfg.MQTT.ClientID)
@@ -29,7 +26,7 @@ func InitMQTTClient() {
 	opts.SetPingTimeout(time.Second)
 	opts.SetConnectTimeout(time.Second)
 	opts.SetWriteTimeout(time.Second)
-	opts.SetKeepAlive(10)
+	opts.SetKeepAlive(10 * time.Second)
 
 	opts.SetAutoReconnect(true)
 	opts.SetConnectRetry(true)
@@ -51,6 +48,15 @@ func InitMQTTClient() {
 		log.WithError(token.Error()).Panic(token.Error())
 	}
 
+	// Listen to construction planning
+	MQTTClient.Subscribe("construction/+/plan", EXACTLY_ONCE, handle_construction_plan)
+	MQTTClient.Subscribe("service/+/plan", EXACTLY_ONCE, handle_service_plan)
+	// Duckie information
+	MQTTClient.Subscribe("duckie/+/status", EXACTLY_ONCE, handle_duckie_status)
+	MQTTClient.Subscribe("obstruction/+/status", EXACTLY_ONCE, handle_obstacle_status)
+
+	log.Info("MQTT client initialized")
+
 	// Messages will be delivered asynchronously so we just need to wait for a signal to shutdown
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
@@ -60,4 +66,51 @@ func InitMQTTClient() {
 	fmt.Println("signal caught - exiting")
 	MQTTClient.Disconnect(1000)
 	fmt.Println("shutdown complete")
+}
+
+func handle_construction_plan(client mqtt.Client, msg mqtt.Message) {
+	log.Info("construction plan received")
+	// TODO - handle construction planning and store it in the database
+	log.Infof("%s", msg.Topic())
+	log.Infof("%s", msg.Payload())
+
+	time.Sleep(10 * time.Second)
+	publish_construction(client, uuid.New())
+}
+
+func handle_service_plan(client mqtt.Client, msg mqtt.Message) {
+	log.Info("service plan received")
+	// TODO - handle construction planning and store it in the database
+	log.Infof("%s", msg.Topic())
+	log.Infof("%s", msg.Payload())
+
+	// Simulate the placement of a service
+	time.Sleep(10 * time.Second)
+	publish_service(client, uuid.New())
+}
+
+func handle_duckie_status(client mqtt.Client, msg mqtt.Message) {
+	log.Info("duckie status received")
+	// TODO - handle construction planning and store it in the database
+	log.Infof("%s", msg.Topic())
+	log.Infof("%s", msg.Payload())
+}
+
+func handle_obstacle_status(client mqtt.Client, msg mqtt.Message) {
+	log.Info("obstacle status received")
+	// TODO - handle construction planning and store it in the database
+	log.Infof("%s", msg.Topic())
+	log.Infof("%s", msg.Payload())
+}
+
+func publish_construction(client mqtt.Client, id uuid.UUID) {
+	topic := fmt.Sprintf("construction/%s/plan", id)
+	// TODO add JSON payload
+	client.Publish(topic, EXACTLY_ONCE, true, "test")
+}
+
+func publish_service(client mqtt.Client, id uuid.UUID) {
+	topic := fmt.Sprintf("service/%s/plan", id)
+	// TODO add JSON payload
+	client.Publish(topic, EXACTLY_ONCE, true, "test")
 }
